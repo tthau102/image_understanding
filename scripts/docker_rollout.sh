cd3974844193d505a8f88d150c047532db2b6a88
tthau102 <trunghautran0102@gmail.com>
2025-07-24 10:42:30 +0700

    Update .gitignore rules with new ignore paths and patterns

---
+++ scripts/docker_rollout.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+IMAGE_NAME="tdhung-review-ui"
+
+VERSION=$(date +"%d.%m.%y.%H.%M.%S")
+
+IMAGE_TAG=$VERSION
+
+# Build và tag image
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
+
+# Stop và remove container cũ
+docker stop $IMAGE_NAME
+docker rm $IMAGE_NAME
+
+# Sử dụng environment variables cho AWS credentials
+docker run -dt -p 8081:8080 \
+  -e AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) \
+  -e AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) \
+  -e AWS_SESSION_TOKEN=$(aws configure get aws_session_token) \
+  -e AWS_REGION=$(aws configure get region) \
+  --name $IMAGE_NAME $IMAGE_NAME
+
+sleep 1
+docker logs $IMAGE_NAME
